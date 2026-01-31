a8e1543feaa8c18b7a227262c77be5cdd13cc0ac
Ashraf <md6162790@gmail.com>
2026-01-31 10:24:56 +0530

    Add files via upload

---
+++ ChapterFive.java
@@ -0,0 +1,106 @@
+public class ChapterFive {
+    public static void main(String[] args) {
+      /*   int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        } 
+       for( int i=0;i<=10;i++){
+        System.out.println(i);
+       } 
+      int i=0;
+      do{
+        System.out.println(i);
+        i++;
+      }while(i<=10);
+      int i=10;
+        while(i>=0){
+            System.out.println(i);
+            i--;
+        }
+       for( int i=10;i>=0;i--){
+        System.out.println(i);
+    }
+   int i=10;
+      do{
+        System.out.println(i);
+        i--;
+      }while(i>=1);
+  int i=0;
+  while(i<=10){
+    System.out.println(i);
+    if(i==5){
+      break;
+    }
+    i++;
+    for(int i=0;i<=10;i++){
+      System.out.println(i);
+      if(i==4){
+        break;
+        int i=0;
+        do{
+          System.out.println(i);
+          if(i==6){
+            break;
+          }
+          i++;
+        }while(i<=10);
+        int i=0;
+        while(i<=10){
+          i++;
+          if(i==4){
+            continue;
+          }
+          System.out.println(i);
+        }
+       int i=1;
+       do{
+          i++;
+          if(i==7){
+            continue;
+          }
+          System.out.println(i);
+       }while(i<=10);
+       for(int i=0;i<=10;i++){
+        if(i==9){
+          continue;
+        }
+        System.out.println(i);
+       }
+      int n=4;
+      for( int i=n;i>0;i--){
+        for(int j=0;j<i;j++){
+          System.out.print("*");
+        }
+        System.out.print("\n");
+      }
+     int sum=0;
+     int n=2;
+     for(int i=0;i<=n;i++)
+      sum=sum+(2*i);
+      System.out.println(sum);
+      int  n=5;
+      for(int i=10;i>=1;i--){
+        System.out.format("%d X %d=%d\n",n,i,i*n);
+        int n=5;
+        int i=1;
+        int fact=1;
+        while(i<=n){
+          
+          fact*=i;
+          i++;
+        }
+        System.out.println(fact);*/
+        int n=6;
+        int fact=1;
+        for(int i=1;i<=n;i++){
+          fact*=i;
+        }
+        System.out.println(fact);
+      }
+}
+    
+  
+  
+  
+

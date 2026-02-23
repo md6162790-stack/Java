cd7b0c734199af600df2cf4cd3ca1f5721eb4966
Ashraf <md6162790@gmail.com>
2026-02-23 23:08:15 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,66 @@
+public class Operators{
+    public static void main(String[] args) {
+        //arithmetic operators
+       /* int a=12;
+        int b=6;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);  
+
+        //relational operators
+        int a=20;
+        int b=23;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b); 
+
+        //logical operators
+        int age=18;
+        System.out.println(age>17 && age<20);
+        System.out.println(age>17 && age<17);
+        System.out.println(age >15 ||age<16);
+        System.out.println(~age); 
+
+        //unary operator
+        int c=12;
+        System.out.println(+c);
+        System.out.println(-c);
+        System.out.println(++c);
+        System.out.println(c++);
+        System.out.println(--c);
+        System.out.println(c--); 
+
+        //assignment operators
+        int a=12;
+        System.out.println(a+=2);
+        System.out.println(a-=2);
+        System.out.println(a*=2);
+        System.out.println(a/=2);
+        System.out.println(a%=2);
+
+        //bitwise operator
+        int a=5;
+        int b=3;
+        System.out.println(a & b);
+        System.out.println(a|b);
+        System.out.println(~5); 
+
+        //shift operator
+        int a=6;
+        System.out.println(a>>1);
+        System.out.println(a<<1); */
+
+        //turnary operator
+        int a=10;
+        int b=20;
+        int max=(a>b)?a:b;
+        System.out.println(max);
+
+
+    }
+}

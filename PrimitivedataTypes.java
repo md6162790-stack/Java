ca70dfdd644222e7734e96f6812d141d7c97ab2a
Ashraf <md6162790@gmail.com>
2026-02-23 22:49:09 +0530

    Add files via upload

---
+++ PrimitivedataTypes.java
@@ -0,0 +1,17 @@
+public class PrimitivedataTypes {
+    public static void main(String[] args) {
+        byte b=12;
+        short s=1200;
+        int i=50000;
+        long l=1234567;
+        double d=12.2356666d;
+        float f=12.12345f;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(f);
+
+    }
+}

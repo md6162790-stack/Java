ed43da8563826c21d84ec79c6fde6ad8ffad1bd0
Ashraf <md6162790@gmail.com>
2026-02-28 11:33:16 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,38 @@
+public class Loops {
+    public static void main(String[] args) {
+        // while loop
+      /*  int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+
+       // for loop
+       for(int i=1;i<=10;i++){
+        System.out.println(i);
+       }  
+      //do while loop
+      int i=1;
+      do{
+        System.out.println(i);
+        i++;
+      }while(i<=10); 
+       
+      // revrsing while loops
+      int i=10;
+      while(i>=1){
+        System.out.println(i);
+        i--;
+      } 
+     //reverse for loop
+     for(int i=10;i>=1;i--){
+        System.out.println(i);
+     } */
+    //reverse do while loop
+    int i=10;
+    do{
+        System.out.println(i);
+        i--;
+    }while(i>=1);
+    }
+}

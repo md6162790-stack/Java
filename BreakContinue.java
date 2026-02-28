b8e93bd06d2f7c70bc9c20d519c1ac57f6ad5d9d
Ashraf <md6162790@gmail.com>
2026-02-28 11:34:08 +0530

    Add files via upload

---
+++ BreakContinue.java
@@ -0,0 +1,52 @@
+public class BreakContinue {
+    public static void main(String[] args) {
+        // break statement
+      /*  int i=1;
+        while(i<=10){
+            System.out.println(i);
+             
+            if(i==5){
+                break;    
+            } i++;
+        }  
+        for(int i=10;i>=1;i--){
+            System.out.println(i);
+            if(i==3){
+                break;
+            } 
+        } 
+       int i=1;
+       do{
+        System.out.println(i);
+        if(i==6){
+            break;
+        }i++;
+       }while(i<=10); 
+
+       //continue
+       int i=0;
+        while(i<=10){
+          i++;
+          if(i==4){
+            continue;
+          }
+          System.out.println(i);
+        } 
+        
+          for(int i=1;i<=10;i++){
+            if(i==7){
+                continue;
+            }
+            System.out.println(i);
+          } */
+
+            int i=1;
+            do{
+                i++;
+                if(i==6){
+                    continue;
+                }
+                System.out.println(i);
+            }while(i<=10);
+    }
+}

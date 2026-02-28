a4bde38ad6154f5824d803fb3c5b5a4ec99bc1e1
Ashraf <md6162790@gmail.com>
2026-02-28 16:09:04 +0530

    Add files via upload

---
+++ LoopsPractice.java
@@ -0,0 +1,32 @@
+public class LoopsPractice {
+    public static void main(String[] args) {
+        //table
+    /*    int n=5;
+        int i=1;
+        for(i=10;i>=1;i--){
+            System.out.format("%d X %d = %d\n",n,i,n*i);
+        }  
+           // sum of  n even numbers
+       int n=3;
+       int sum=0;
+       for(int i=1;i<n;i++){
+        sum=sum+(2*i);
+       }
+       System.out.println(sum);
+        // factorial 
+       int n=5;
+       int i=1;
+       int fact=1;;
+        while(i<=n){
+            fact*=i;
+            i++;
+        }
+        System.out.println(fact);
+        int n=8;
+        int sum=0;
+        for(int i=1;i<=10;i++){
+            sum+=sum+n*i;
+        }
+        System.out.println(sum);  */
+    }
+}
